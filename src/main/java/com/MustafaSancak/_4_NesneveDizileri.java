ea6ee87aa9f177d07101fa6009127e3c5578d098
Mustafa Sancak <mustafa.sancak2010@gmail.com>
2024-07-29 18:50:26 +0300

    Dizi oluşturuldu

---
+++ src/main/java/com/MustafaSancak/_4_NesneveDizileri.java
@@ -0,0 +1,16 @@
+package com.MustafaSancak;
+
+public class _4_NesneveDizileri {
+    public static void main(String[] args) {
+        // Bir dizi nesne oluşturalım
+        String[] strings = new String[3];
+
+        // Her bir dizi elemanına nesne atayalım
+        strings[0] = "Java";
+        strings[1] = "Python";
+        strings[2] = "C++";
+
+        System.out.println(strings[2]);
+
+    }
+}
