aa6b13a99a8f19f3d97f7ae7b41936d83bcc5241
Mustafa Sancak <mustafa.sancak2010@gmail.com>
2024-07-29 23:34:43 +0300

    Primitive and Wrapper Class

---
+++ src/main/java/com/MustafaSancak/_5_Örnekler.java
@@ -0,0 +1,24 @@
+package com.MustafaSancak;
+
+public class _5_Örnekler {
+    public static void main(String[] args) {
+
+        // tamsayılarla işlemler
+
+        byte a=127;
+        short b=10000;
+        int c=600000000;
+        long d=2000000000;
+
+        // Kayan noktalı sayılar
+
+        float f1=100.5f; // 32 bit
+        double f2=5004.7;// 64 bit
+
+        // Primitivi Wrappera çevirmek
+        int number=44;
+        Integer numberyeni=number;
+
+
+    }
+}
