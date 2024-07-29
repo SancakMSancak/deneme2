533da5b8575fce2002dc01dd7f198abad12a90a0
Mustafa Sancak <mustafa.sancak2010@gmail.com>
2024-07-29 11:50:12 +0300

    Java Operators, psvm

---
+++ src/main/java/com/MustafaSancak/_3_WrapperYapısı.java
@@ -0,0 +1,15 @@
+package com.MustafaSancak;
+
+public class _3_WrapperYapısı {
+    public static void main(String[] args) {
+        Integer sayi;
+        sayi = 0;
+
+        char a;
+        a = 'A';
+        System.out.println(sayi);
+        System.out.println(a);
+
+
+    }
+}
