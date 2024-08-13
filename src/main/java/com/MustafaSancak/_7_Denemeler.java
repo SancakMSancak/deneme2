c183820604b1b57dd6727e4776dbd2c42dc14e9d
Mustafa Sancak <mustafa.sancak2010@gmail.com>
2024-08-13 20:11:04 +0300

    null point hatası örneği

---
+++ src/main/java/com/MustafaSancak/_7_Denemeler.java
@@ -0,0 +1,16 @@
+package com.MustafaSancak;
+
+public class _7_Denemeler {
+    public static void main(String[] args) {
+        String bosluk = null;
+
+        // null pointer error: nullPointerException
+        Integer sayi1 = null,
+                sayi2 = 20;
+
+        System.out.println(bosluk);
+        System.out.println(" ");
+        System.out.println("sonuc" + (sayi1 + sayi2));
+
+    }
+}
